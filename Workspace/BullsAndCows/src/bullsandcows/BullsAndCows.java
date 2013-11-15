fb34f653a6a762489bb22e9de10a7cf982de1ff2
osfprieto <osfprieto@gmail.com>
2013-11-15 07:53:35 -0500

    Bulls and cows code set to OOP and game set on BullsAndCows class with a player

--- Workspace/BullsAndCows/src/bullsandcows/BullsAndCows.java
+++ Workspace/BullsAndCows/src/bullsandcows/BullsAndCows.java
@@ -4,6 +4,8 @@
  */
 package bullsandcows;
 
+import java.util.Scanner;
+
 /**
  *
  * @author osfprieto
@@ -14,6 +16,42 @@ public class BullsAndCows {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Bulls and Cows");
+        System.out.println("==============");
+        System.out.println();
         
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Answer length:");
+        int answerSize = sc.nextInt();
+        
+        System.out.println("Digits:");
+        int digits = sc.nextInt();
+
+        UNEquipoPlayer player = new UNEquipoPlayer(answerSize, digits);
+
+        while(player.hasToAsk()){
+            String playerGuess = player.getGuess();
+            try{
+                
+                System.out.println("The guess is "+playerGuess);
+                
+                System.out.println("How many bulls?");
+                int bulls = sc.nextInt();
+                
+                System.out.println("How many cows?");
+                int cows = sc.nextInt();
+                
+                player.registerBullsAndCows(playerGuess, bulls, cows);
+            }catch(Exception e){
+                System.out.println("Fail. Try again");
+            }
+        }
+
+        if(player.hasBeenCheated())
+            System.out.println("You're cheating!");
+        else
+            System.out.println("The secret number is "+player.getGuess()+
+                    ". Guesses done: "+player.getGuessesDone());
     }
 }
--- Workspace/BullsAndCows/src/bullsandcows/UNEquipoPlayer.java
+++ Workspace/BullsAndCows/src/bullsandcows/UNEquipoPlayer.java
@@ -8,97 +8,95 @@ package bullsandcows;
 
 import java.util.LinkedList;
 import java.util.Random;
-import java.util.Scanner;
 
 
-public class UNEquipoPlayer <T>{
+public class UNEquipoPlayer{
    
-        static final int ANSWER_SIZE = 4;
- 
-        static LinkedList<String> Permutations(int size)
-        {
-            if (size > 0)
-            {
-                LinkedList ret = new LinkedList<String>();
-                for (String s : Permutations(size - 1))
-                    for (char n : "1234567890".toCharArray())
-                        if (!s.contains(n+""))
-                            ret.add(s + n);
-                return ret;
-            }
-            else{
-                LinkedList<String> ret = new LinkedList<String>();
-                ret.add("");
-                return ret;
-                
-            }
-        }
- 
-        static LinkedList<String> Shuffle(LinkedList<String> source)
-        {
-            LinkedList<String> ret = new LinkedList<String>();
-            Random random = new Random();
-            
-            LinkedList<String> list = (LinkedList<String>)source.clone();
-            while (list.size() > 0)
-            {
-                int ix = random.nextInt(list.size());
-                ret.add(list.get(ix));
-                list.remove(ix);
-            }
+        
+    private int answerSize;
+    private int digits;
+    private LinkedList<String> answers;
+    private Random random;
+    private int guessesDone;
+    private char digitsArray[];
+
+    public UNEquipoPlayer(int answerSize, int digits){
+        this.answerSize = answerSize;
+        this.digits = digits;
+        init();
+    }
+
+    private void init(){
+
+        digitsArray = new char[digits];
+        digitsArray[0] = '0';
+        for(int i=1;i<digits;i++)
+            digitsArray[i] = (char)(digitsArray[i-1]+1);
+
+        random = new Random();
+        answers = Shuffle(Permutations(answerSize));
+        guessesDone = 0;
+
+    }
+
+    private LinkedList<String> Permutations(int size){
+        if (size > 0){
+            LinkedList ret = new LinkedList<String>();
+
+            for (String s : Permutations(size - 1))
+                for (char n : digitsArray)
+                    if (!s.contains(n+""))
+                        ret.add(s + n);
             return ret;
         }
- 
-        static int bulls;
-        static int cows;
-        
-        static boolean ReadBullsCows()
-        {
-            Scanner sc = new Scanner(System.in);
-            String[] input = sc.nextLine().trim().split(",");
-            bulls = cows = 0;
-            if (input.length < 2)
-                return false;
-            else
-                try{
-                    bulls = Integer.parseInt(input[0]);
-                    cows = Integer.parseInt(input[1]);
-                    return true;
-                }catch(Exception e){
-                    return false;
-                }
+        else{
+            LinkedList<String> ret = new LinkedList<String>();
+            ret.add("");
+            return ret;
         }
- 
-        public static void main(String[] args)
-        {
-            System.out.println("Bulls and Cows");
-            System.out.println("==============");
-            System.out.println();
-            LinkedList<String> answers = Shuffle(Permutations(ANSWER_SIZE));
-            
-            while (answers.size() > 1)
-            {
-                String guess = answers.get(0);
-                System.out.println("My guess is "+guess+". How many bulls, cows? ");
-                if (!ReadBullsCows())
-                    System.out.println("Sorry, I didn't understand that. Please try again.");
-                else
-                    for (int ans = answers.size() - 1; ans >= 0; ans--)
-                    {
-                        int tb = 0, tc = 0;
-                        for (int ix = 0; ix < ANSWER_SIZE; ix++)
-                            if (answers.get(ans).charAt(ix) == guess.charAt(ix))
-                                tb++;
-                            else if (answers.get(ans).contains(""+guess.charAt(ix)))
-                                tc++;
-                        if ((tb != bulls) || (tc != cows))
-                            answers.remove(ans);
-                    }
-            }
-            if (answers.size() == 1)
-                System.out.println("Hooray! The answer is "+answers.get(0)+"!");
-            else
-                System.out.println("No possible answer fits the scores you gave.");
+    }
+
+    private LinkedList<String> Shuffle(LinkedList<String> source){
+        LinkedList<String> ret = new LinkedList<String>();
+
+        while (source.size() > 0){
+            int ix = random.nextInt(source.size());
+            ret.add(source.get(ix));
+            source.remove(ix);
         }
+        return ret;
+    }
+
+    public void registerBullsAndCows(String guess, int bulls, int cows){
+
+        for (int ans = answers.size() - 1; ans >= 0; ans--){
+            int tb = 0, tc = 0;
+            for (int ix = 0; ix < answerSize; ix++)
+                if (answers.get(ans).charAt(ix) == guess.charAt(ix))
+                    tb++;
+                else if (answers.get(ans).contains(""+guess.charAt(ix)))
+                    tc++;
+            if ((tb != bulls) || (tc != cows))
+                answers.remove(ans);
+        }
+
+    }
+
+    public String getGuess(){
+        guessesDone++;
+        return answers.get(random.nextInt(answers.size()));
+    }
+
+    public boolean hasToAsk(){
+        return answers.size()>1;
+    }
+
+    public boolean hasBeenCheated(){
+        return answers.size()==0;
+    }
+
+    public int getGuessesDone(){
+        return guessesDone;
+    }
 }
     
